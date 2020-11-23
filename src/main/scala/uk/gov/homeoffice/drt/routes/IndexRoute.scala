8759735dd4c0eb1a3485a69b27631051919208ed
Rich Birch <ringo@albumsnaps.com>
2020-11-23 13:14:12 +0000

    DRTII-649 Log referer when there is one

--- src/main/scala/uk/gov/homeoffice/drt/routes/IndexRoute.scala
+++ src/main/scala/uk/gov/homeoffice/drt/routes/IndexRoute.scala
@@ -1,14 +1,23 @@
 package uk.gov.homeoffice.drt.routes
 
-import akka.http.scaladsl.server.Directives.{ concat, getFromResource, getFromResourceDirectory, path, pathPrefix }
+import akka.http.scaladsl.server.Directives.{concat, getFromResource, getFromResourceDirectory, optionalHeaderValueByName, path, pathPrefix}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.get
+import org.slf4j.{Logger, LoggerFactory}
 
 object IndexRoute {
+  val log: Logger = LoggerFactory.getLogger(getClass)
+
   def apply(): Route = {
     concat(
       path("") {
-        getFromResource("frontend/index.html")
+        optionalHeaderValueByName("") {
+          case Some(referer) =>
+            log.info(s"Referer: $referer")
+            getFromResource("frontend/index.html")
+          case _ =>
+            getFromResource("frontend/index.html")
+        }
       },
       (get & pathPrefix("")) {
         getFromResourceDirectory("frontend")
