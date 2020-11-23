25c8c8fc075db343f01e6fba53869095f7fe79f7
Rich Birch <ringo@albumsnaps.com>
2020-11-23 13:51:18 +0000

    DRTII-649 Log all headers

--- src/main/scala/uk/gov/homeoffice/drt/routes/IndexRoute.scala
+++ src/main/scala/uk/gov/homeoffice/drt/routes/IndexRoute.scala
@@ -1,23 +1,25 @@
 package uk.gov.homeoffice.drt.routes
 
-import akka.http.scaladsl.server.Directives.{concat, getFromResource, getFromResourceDirectory, optionalHeaderValueByName, path, pathPrefix}
+import akka.http.scaladsl.server.Directives.{ concat, getFromResource, getFromResourceDirectory, optionalHeaderValueByName, path, pathPrefix }
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.get
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.{ Logger, LoggerFactory }
 
 object IndexRoute {
   val log: Logger = LoggerFactory.getLogger(getClass)
 
   def apply(): Route = {
     concat(
-      path("") {
-        optionalHeaderValueByName("Referer") {
-          case Some(referer) =>
-            log.info(s"Referer: $referer")
-            getFromResource("frontend/index.html")
-          case _ =>
-            getFromResource("frontend/index.html")
-        }
+      path("") { r =>
+        //        optionalHeaderValueByName("Referer") {
+        //          case Some(referer) =>
+        //            log.info(s"Referer: $referer")
+        //            getFromResource("frontend/index.html")
+        //          case _ =>
+        r.request.headers.foreach(h => log.info(s"header: ${h.name()}: ${h.value()}"))
+        //            getFromResource("frontend/index.html")
+        r.complete("yeah")
+        //        }
       },
       (get & pathPrefix("")) {
         getFromResourceDirectory("frontend")
