0106903942db2ee18da49e3a68cc8d933a5571ab
Nilesh Gupta <guptanilesh13@gmail.com>
2022-10-17 13:15:35 +0100

    DRTII-1034 update usermanagement route at server (#79)
    Co-authored-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- src/main/scala/uk/gov/homeoffice/drt/routes/IndexRoute.scala
+++ src/main/scala/uk/gov/homeoffice/drt/routes/IndexRoute.scala
@@ -1,10 +1,10 @@
 package uk.gov.homeoffice.drt.routes
 
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives.{ concat, optionalHeaderValueByName, parameterMap, path, pathPrefix, redirect }
+import akka.http.scaladsl.server.Directives.{concat, optionalHeaderValueByName, parameterMap, path, pathPrefix, redirect}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.get
-import org.slf4j.{ Logger, LoggerFactory }
+import org.slf4j.{Logger, LoggerFactory}
 import uk.gov.homeoffice.drt.Urls
 import uk.gov.homeoffice.drt.auth.Roles.NeboUpload
 import uk.gov.homeoffice.drt.authentication.User
@@ -18,6 +18,8 @@ case object AlertPathString extends PathString
 case object UploadPathString extends PathString
 
 case object RegionPathString extends PathString
+
+case object UserManagementPathString extends PathString
 
 case class IndexRoute(urls: Urls, indexResource: Route, directoryResource: Route, staticResourceDirectory: Route) {
   val log: Logger = LoggerFactory.getLogger(getClass)
@@ -35,6 +37,9 @@ case class IndexRoute(urls: Urls, indexResource: Route, directoryResource: Route
       },
       path("region") {
         indexRouteDirectives(RegionPathString)
+      },
+      path("userManagement") {
+        indexRouteDirectives(UserManagementPathString)
       },
       (get & pathPrefix("")) {
         directoryResource
