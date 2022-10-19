7674750e15c424afb7a2b915f51fa5164ee73835
Rich Birch <ringo@albumsnaps.com>
2022-10-19 08:39:04 +0100

    DRTII-1103 Send copy of access granted email to drt team for visibility

--- src/main/scala/uk/gov/homeoffice/drt/notifications/EmailNotifications.scala
+++ src/main/scala/uk/gov/homeoffice/drt/notifications/EmailNotifications.scala
@@ -1,11 +1,10 @@
 package uk.gov.homeoffice.drt.notifications
 
 import org.slf4j.{Logger, LoggerFactory}
-
-import java.util
 import uk.gov.homeoffice.drt.authentication.{AccessRequest, ClientUserRequestedAccessData}
 import uk.gov.service.notify.{NotificationClient, SendEmailResponse}
 
+import java.util
 import scala.collection.JavaConverters.mapAsJavaMapConverter
 import scala.util.Try
 
@@ -20,8 +19,6 @@ case class EmailNotifications(apiKey: String, accessRequestEmails: List[String])
 
   val accessGrantedTemplateId = "12e36257-c485-4e13-af4f-2293d2dd34a6"
 
-  val accessGrantedBccTemplateId = "06e88a3b-2375-4acc-9fde-f2b7afce6bb6"
-
   def getFirstName(email: String): String = {
     Try(email.split("\\.").head.toLowerCase.capitalize).getOrElse(email)
   }
@@ -33,9 +30,9 @@ case class EmailNotifications(apiKey: String, accessRequestEmails: List[String])
       s"https://${curad.portsRequested.trim.toLowerCase()}.$domain/"
   }
 
-  def sendAccessGranted(clientUserRequestedAccessData: ClientUserRequestedAccessData, domain: String, teamEmail: String) = {
+  def sendAccessGranted(clientUserRequestedAccessData: ClientUserRequestedAccessData, domain: String, teamEmail: String): Try[SendEmailResponse] = {
     val personalisation: util.Map[String, String] =
-      Map("requester" -> clientUserRequestedAccessData.email,
+      Map(
         "requesterUsername" -> getFirstName(clientUserRequestedAccessData.email),
         "link" -> getLink(clientUserRequestedAccessData, domain),
       ).asJava
@@ -43,15 +40,17 @@ case class EmailNotifications(apiKey: String, accessRequestEmails: List[String])
       accessGrantedTemplateId,
       clientUserRequestedAccessData.email,
       personalisation,
-      "access granted")).recover {
+      "access granted")
+    ).recover {
       case e => log.error(s"Error while sending email to requester ${clientUserRequestedAccessData.email} for grant access confirmation")
         throw e
     }
     Try(client.sendEmail(
-      accessGrantedBccTemplateId,
+      accessGrantedTemplateId,
       teamEmail,
       personalisation,
-      "access granted bcc")).recover {
+      "access granted bcc")
+    ).recover {
       case e => log.error(s"Error while sending bcc email to team $teamEmail for requester ${clientUserRequestedAccessData.email} for grant access confirmation")
         throw e
     }
