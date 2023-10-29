07d2b185366102e488974fd94dd817c281b99ccb
Rich Birch <rich@freshcodelimited.com>
2023-10-29 21:23:58 +0000

    DRTII-1305 Switch printlns to log messages

--- src/main/scala/uk/gov/homeoffice/drt/healthchecks/HealthCheckMonitor.scala
+++ src/main/scala/uk/gov/homeoffice/drt/healthchecks/HealthCheckMonitor.scala
@@ -4,12 +4,15 @@ import akka.Done
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Sink, Source}
+import org.slf4j.LoggerFactory
 import uk.gov.homeoffice.drt.healthchecks.alarms.AlarmState
 import uk.gov.homeoffice.drt.ports.PortCode
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object HealthCheckMonitor {
+  private val log = LoggerFactory.getLogger(getClass)
+
   def apply(makeRequest: HttpRequest => Future[HttpResponse],
             recordResponse: (PortCode, HealthCheckResponse[_]) => Future[AlarmState],
             ports: Iterable[PortCode]
@@ -17,7 +20,7 @@ object HealthCheckMonitor {
            (implicit mat: Materializer, ec: ExecutionContext): () => Future[Done] =
     () => Source(ports.toList)
       .mapAsync(1) { port =>
-        println("checking port " + port)
+        log.info("checking port " + port)
         PortHealthCheck(port, makeRequest).map(_.map(r => (port, r)))
       }
       .mapConcat(identity)
@@ -27,7 +30,7 @@ object HealthCheckMonitor {
       .runWith(Sink.ignore)
       .recover {
         case t: Throwable =>
-          println("HealthCheckMonitor failed: " + t.getMessage)
+          log.error("HealthCheckMonitor failed: " + t.getMessage)
           Done
       }
 }
