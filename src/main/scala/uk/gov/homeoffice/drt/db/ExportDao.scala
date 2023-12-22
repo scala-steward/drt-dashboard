eb98fd3ba8defbd891288656df0ecaa81c0a5fe3
Rich Birch <ringo@albumsnaps.com>
2023-12-22 08:32:35 +0000

    DRTII-1381 Reinstate overwritten ExportDao.. oops

--- src/main/scala/uk/gov/homeoffice/drt/db/ExportDao.scala
+++ src/main/scala/uk/gov/homeoffice/drt/db/ExportDao.scala
@@ -1,58 +1,80 @@
 package uk.gov.homeoffice.drt.db
 
+import slick.dbio.Effect
 import slick.lifted.Tag
+import slick.sql.FixedSqlAction
 import uk.gov.homeoffice.drt.db.Db.slickProfile.api._
-import uk.gov.homeoffice.drt.time.{LocalDate, SDate, UtcDate}
+import uk.gov.homeoffice.drt.models.Export
+import uk.gov.homeoffice.drt.time.{LocalDate, SDate}
 
 import java.sql.Timestamp
 import scala.concurrent.ExecutionContext
 
-class PassengersHourlyTable(tag: Tag)
-  extends Table[(String, Int, String, String, Int, Timestamp, Timestamp)](tag, "passengers_hourly") {
+class ExportTable(tag: Tag)
+  extends Table[(String, String, String, String, String, Timestamp)](tag, "export") {
 
-  def dateUtc: Rep[String] = column[String]("date_utc")
+  def email: Rep[String] = column[String]("email")
 
-  def hour: Rep[Int] = column[Int]("hour")
+  def terminals: Rep[String] = column[String]("terminals")
 
-  def port: Rep[String] = column[String]("port")
+  private def startDate: Rep[String] = column[String]("start_date")
 
-  def terminal: Rep[String] = column[String]("terminal")
+  private def endDate: Rep[String] = column[String]("end_date")
 
-  def passengers: Rep[Int] = column[Int]("passengers")
+  def status: Rep[String] = column[String]("status")
 
   def createdAt: Rep[Timestamp] = column[java.sql.Timestamp]("created_at")
 
-  def updatedAt: Rep[Timestamp] = column[java.sql.Timestamp]("updated_at")
+  def pk = primaryKey("pk_region_export_created", (email, terminals, createdAt))
 
-  def pk = primaryKey("pk_date_utc_hour_port_terminal", (dateUtc, hour, port, terminal))
-
-  def * = (dateUtc, hour, port, terminal, passengers, createdAt, updatedAt)
+  def * = (email, terminals, startDate, endDate, status, createdAt)
 }
 
-object PassengersHourlyQueries {
-  val table: TableQuery[PassengersHourlyTable] = TableQuery[PassengersHourlyTable]
+object ExportQueries {
+  val regionExports: TableQuery[ExportTable] = TableQuery[ExportTable]
 
-  def getTotalForPortAndDate(port: String)
-                            (implicit ec: ExecutionContext):
-  LocalDate => DBIOAction[Int, NoStream, Effect.Read] =
-    localDate => {
-      val sdate = SDate(localDate)
-      val utcDates = Set(
-        sdate.getLocalLastMidnight.toUtcDate,
-        sdate.getLocalNextMidnight.toUtcDate,
-      )
+  def get(email: String, createdAt: Long)
+         (implicit ec: ExecutionContext): DBIOAction[Option[Export], NoStream, Effect.Read] = {
+    val createdAtTs = new Timestamp(createdAt)
+    regionExports
+      .filter(_.email === email)
+      .filter(_.createdAt === createdAtTs)
+      .result
+      .map(_.headOption.map(x => exportFromRow(x)))
+  }
 
-      table
-        .filter(_.port === port.toLowerCase)
-        .filter(_.dateUtc inSet utcDates.map(_.toISOString))
-        .result
-        .map { rows =>
-          rows.filter {
-            case (utc, hour, _, _, pax, _, _) =>
-              val Array(y, m, d) = utc.split("-").map(_.toInt)
-              val rowLocalDate = SDate(UtcDate(y, m, d)).addHours(hour).toLocalDate
-              rowLocalDate == localDate
-          }.map(_._5).sum
-        }
-    }
-}
+  def getAll(email: String)
+            (implicit ec: ExecutionContext): DBIOAction[Seq[Export], NoStream, Effect.Read] =
+    regionExports
+      .filter(_.email === email)
+      .result
+      .map(_.map(exportFromRow))
+
+  def insert(regionExport: Export): DBIOAction[Int, NoStream, Effect.Write] = {
+    val (startDate: String, endDate: String, createdAt: Timestamp) = dates(regionExport)
+    regionExports += (regionExport.email, regionExport.terminals, startDate, endDate, regionExport.status, createdAt)
+  }
+
+  def update(regionExport: Export): FixedSqlAction[Int, NoStream, Effect.Write] = {
+    val query = for {
+      export <- regionExports if matches(regionExport, export)
+    } yield export.status
+
+    query.update(regionExport.status)
+  }
+
+  private def matches(regionExport: Export, export: ExportTable): Rep[Boolean] = {
+    export.email === regionExport.email && export.terminals === regionExport.terminals && export.createdAt === new Timestamp(regionExport.createdAt.millisSinceEpoch)
+  }
+
+  private def exportFromRow(row: (String, String, String, String, String, Timestamp)): Export = {
+    val startDate = LocalDate.parse(row._3).getOrElse(throw new Exception(s"Could not parse date ${row._3}"))
+    val endDate = LocalDate.parse(row._4).getOrElse(throw new Exception(s"Could not parse date ${row._4}"))
+    val createdAt = SDate(row._6.getTime)
+    Export(row._1, row._2, startDate, endDate, row._5, createdAt)
+  }
+
+  private def dates(regionExport: Export): (String, String, Timestamp) = {
+    val createdAt = new Timestamp(regionExport.createdAt.millisSinceEpoch)
+    (regionExport.startDate.toISOString, regionExport.endDate.toISOString, createdAt)
+  }
