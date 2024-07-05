db8cdf5bc8b9ba6485619779a94758ae701e44fc
Rich Birch <ringo@albumsnaps.com>
2024-07-05 11:25:54 +0100

    DRTII-1521 Rewrite end point to directly query the db

--- src/main/scala/uk/gov/homeoffice/drt/routes/PassengerRoutes.scala
+++ src/main/scala/uk/gov/homeoffice/drt/routes/PassengerRoutes.scala
@@ -1,26 +1,30 @@
 package uk.gov.homeoffice.drt.routes
 
+import akka.NotUsed
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
-import akka.util.ByteString
+import akka.stream.scaladsl.Source
 import org.slf4j.LoggerFactory
-import uk.gov.homeoffice.drt.jsonformats.PassengersSummaryFormat.JsonFormat
+import spray.json.enrichAny
 import uk.gov.homeoffice.drt.models.{PassengersSummaries, PassengersSummary}
-import uk.gov.homeoffice.drt.ports.PortCode
-import uk.gov.homeoffice.drt.time.LocalDate
-import uk.gov.homeoffice.drt.{Dashboard, HttpClient}
+import uk.gov.homeoffice.drt.ports.Queues.Queue
+import uk.gov.homeoffice.drt.ports.Terminals.Terminal
+import uk.gov.homeoffice.drt.ports.{PortCode, PortRegion, Queues}
+import uk.gov.homeoffice.drt.services.PassengerSummaryStreams
+import uk.gov.homeoffice.drt.services.PassengerSummaryStreams.{Granularity, Total}
+import uk.gov.homeoffice.drt.time.TimeZoneHelper.europeLondonTimeZone
+import uk.gov.homeoffice.drt.time.{LocalDate, SDate}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.Success
 
 
 object PassengerRoutes {
   private val log = LoggerFactory.getLogger(getClass)
 
-  def apply(httpClient: HttpClient)
+  def apply(passengerSummaries: PassengerSummaryStreams)
            (implicit ec: ExecutionContext, mat: Materializer): Route =
     pathPrefix("passengers" / Segment / Segment) {
       case (startDate, endDate) =>
@@ -28,37 +32,55 @@ object PassengerRoutes {
           val portCodes = portCodesStr.split(",")
           concat(
             pathEnd(
-              passengersForPort(httpClient, portCodes, startDate, endDate, None)
+              passengersForPort(portCodes, startDate, endDate, None, passengerSummaries)
             ),
             path(Segment)(terminal =>
-              passengersForPort(httpClient, portCodes, startDate, endDate, Some(terminal))
+              passengersForPort(portCodes, startDate, endDate, Some(terminal), passengerSummaries)
             )
           )
         }
     }
 
-  private def passengersForPort(httpClient: HttpClient, portCodes: Iterable[String], startDate: String, endDate: String, maybeTerminal: Option[String])
+  private def passengersForPort(portCodes: Iterable[String],
+                                startDate: String,
+                                endDate: String,
+                                maybeTerminal: Option[String],
+                                passengerSummaries: PassengerSummaryStreams,
+                               )
                                (implicit ec: ExecutionContext, mat: Materializer): Route = {
     get {
       extractRequest { request =>
         parameters("granularity".optional) { maybeGranularity =>
-          val requests = for {
-            start <- LocalDate.parse(startDate).toList
-            end <- LocalDate.parse(endDate).toList
-            portCode <- portCodes
-          } yield {
-            val incomingHeaders = request.headers
-            val url = endpointUrl(PortCode(portCode), start, end, maybeTerminal, maybeGranularity.getOrElse("total"))
-
-            HttpRequest(uri = url, headers = incomingHeaders)
+          val (start, end) = (LocalDate.parse(startDate), LocalDate.parse(endDate)) match {
+            case (Some(s), Some(e)) => (s, e)
+            case _ => throw new IllegalArgumentException(s"Invalid date range: $startDate - $endDate")
           }
+          val granularity = maybeGranularity.map(Granularity.fromString).getOrElse(Total)
+          val streamForPort = passengerSummaries.streamForGranularity(start, end, granularity, maybeTerminal.map(Terminal(_)))
 
           val contentType = request.headers.find(_.name() == "Accept").map {
             case header if header.value() == "text/csv" => ContentTypes.`text/csv(UTF-8)`
             case _ => ContentTypes.`application/json`
           }.getOrElse(ContentTypes.`application/json`)
 
-          val eventualContent = requestsToPassengerSummaries(httpClient, contentType, requests)
+          val portResult: Source[(PortCode, (Map[Queue, Int], Int, Option[Any])), NotUsed] = Source(portCodes.toList)
+            .flatMapConcat(portCodeStr => {
+              val portCode = PortCode(portCodeStr)
+              streamForPort(portCode).map(result => (portCode, result))
+            })
+
+          val eventualContent = if (contentType == ContentTypes.`text/csv(UTF-8)`)
+            portResult.runFold("") {
+              case (acc, (portCode, (queues, capacity, x))) => acc + passengersCsvRow(portCode, maybeTerminal, queues, capacity, x)
+            }
+          else {
+            import uk.gov.homeoffice.drt.jsonformats.PassengersSummaryFormat._
+            portResult
+              .runFold(PassengersSummaries.empty) {
+                case (acc, (portCode, (queues, capacity, x))) => acc ++ Seq(passengersJson(portCode, maybeTerminal, queues, capacity, x))
+              }
+              .map(_.toJson.compactPrint)
+          }
 
           onComplete(eventualContent) {
             case Success(content) => complete(HttpEntity(contentType, content))
@@ -68,51 +90,39 @@ object PassengerRoutes {
     }
   }
 
-  private def requestsToPassengerSummaries(httpClient: HttpClient,
-                                           contentType: ContentType,
-                                           requests: Iterable[HttpRequest],
-                                  )
-                                          (implicit ec: ExecutionContext, mat: Materializer): Future[String] = {
-    import spray.json.DefaultJsonProtocol._
-    import spray.json._
+  private def passengersCsvRow[T]: (PortCode, Option[String], Map[Queue, Int], Int, Option[T]) => String =
+    (portCode, maybeTerminal, queueCounts, capacity, maybeDateOrDateHour) => {
+      val regionName = PortRegion.fromPort(portCode).name
+      val portCodeStr = portCode.toString
+      val totalPcpPax = queueCounts.values.sum
+      val queueCells = Queues.queueOrder
+        .map(queue => queueCounts.getOrElse(queue, 0).toString)
+        .mkString(",")
 
-    val byteStreams = Source(requests.toList)
-      .mapAsync(1)(httpClient.send(_))
-      .flatMapConcat { response =>
-        response.status match {
-          case StatusCodes.OK =>
-            response.entity.dataBytes
-          case _ =>
-            log.error(s"Failed to get passenger summaries: ${response.status}")
-            Source.empty[ByteString]
-        }
+      val dateStr = maybeDateOrDateHour.map {
+        case date: LocalDate => date.toISOString
+        case date: Long => s"${SDate(date, europeLondonTimeZone).toISOString}"
       }
-    val stringStream = contentType match {
-      case ContentTypes.`text/csv(UTF-8)` =>
-        byteStreams
-          .map(_.utf8String)
-          .fold("")(_ + _)
-      case ContentTypes.`application/json` =>
-        byteStreams
-          .fold(PassengersSummaries.empty) {
-            case (acc, dataBytes) => acc ++ dataBytes.utf8String.parseJson.convertTo[Seq[PassengersSummary]]
-          }
-          .map(_.summaries.toJson.compactPrint)
+      maybeTerminal match {
+        case Some(terminal) =>
+          (dateStr.toList ++ List(regionName, portCodeStr, terminal, capacity, totalPcpPax, queueCells)).mkString(",") + "\n"
+        case None =>
+          (dateStr.toList ++ List(regionName, portCodeStr, capacity, totalPcpPax, queueCells)).mkString(",") + "\n"
+      }
     }
-    stringStream.runWith(Sink.head)
-  }
 
-  private def endpointUrl(rootDomain: PortCode, start: LocalDate, end: LocalDate, maybeTerminal: Option[String], granularity: String): String = {
-    val possibleTerminal = maybeTerminal.map(_.toLowerCase).toList
-
-    val urlParts = Seq(
-      Dashboard.drtInternalUriForPortCode(rootDomain),
-      "api",
-      "passengers",
-      start.toISOString,
-      end.toISOString,
-    ) ++ possibleTerminal
-
-    urlParts.mkString("/") + s"?granularity=$granularity"
-  }
+  private def passengersJson[T]: (PortCode, Option[String], Map[Queue, Int], Int, Option[T]) => PassengersSummary =
+    (portCode, maybeTerminal, queueCounts, capacity, maybeDateOrDateHour) => {
+      val regionName = PortRegion.fromPort(portCode).name
+      val portCodeStr = portCode.toString
+      val totalPcpPax = queueCounts.values.sum
+      val (maybeDate, maybeHour) = maybeDateOrDateHour match {
+        case Some(date: LocalDate) => (Option(date), None)
+        case Some(date: Long) =>
+          val sdate = SDate(date, europeLondonTimeZone)
+          (Option(sdate.toLocalDate), Option(sdate.getHours))
+        case _ => (None, None)
+      }
+      PassengersSummary(regionName, portCodeStr, maybeTerminal, capacity, totalPcpPax, queueCounts, maybeDate, maybeHour)
+    }
 }
